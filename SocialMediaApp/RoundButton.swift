191d5d75679998fae04f15391d3e836ed6b81095
adamseppi <adamseppi@yahoo.com>
2017-05-19 12:25:51 -0500

    Dope UI Created

---
+++ SocialMediaApp/RoundButton.swift
@@ -0,0 +1,31 @@
+//
+//  RoundButton.swift
+//  SocialMediaApp
+//
+//  Created by Adam Seppi on 5/19/17.
+//  Copyright © 2017 AdamSeppi. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class RoundButton: UIButton {
+
+    @IBInspectable var cornerRadius: CGFloat = 0 {
+        didSet{
+            self.layer.cornerRadius = cornerRadius
+        }
+    }
+    
+    @IBInspectable var borderWidth: CGFloat = 0 {
+        didSet{
+            self.layer.borderWidth = borderWidth
+        }
+    }
+    
+    @IBInspectable var borderColor: UIColor = UIColor.black {
+        didSet{
+            self.layer.borderColor = borderColor.cgColor
+        }
+    }
+}
