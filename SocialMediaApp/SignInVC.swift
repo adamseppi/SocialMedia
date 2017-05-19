9f67c1eaba8d8829e55fce2754ffe8154d439582
adamseppi <adamseppi@yahoo.com>
2017-05-19 18:04:08 -0500

    Custom FB button working. Visable through FBSDKLoginButton. Problem with enabling secret key (had to show it to copy) on Firebase Console

--- SocialMediaApp/SignInVC.swift
+++ SocialMediaApp/SignInVC.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 import FBSDKLoginKit
 
 class SignInVC: UIViewController {
@@ -17,10 +18,37 @@ class SignInVC: UIViewController {
         let loginButton = FBSDKLoginButton()
         view.addSubview(loginButton)
         loginButton.frame = CGRect(x: 16, y: 50, width: view.frame.width - 32, height: 50)
-        
-        
     }
 
-
+    @IBAction func FBButtonClicked(_ sender: AnyObject) {
+        
+        let facebookLogin = FBSDKLoginManager()
+        
+        facebookLogin.logIn(withReadPermissions: ["email"], from: self) { (result, error) in
+            if error != nil {
+                print("ADAM: Unable to authenticate with Facebook - \(error)")
+            }
+            else if result?.isCancelled == true {
+                print("ADAM: User Cancelled Auth")
+            }
+            else {
+                print("ADAM: Successfully athenticated with FB")
+                let credential = FacebookAuthProvider.credential(withAccessToken: FBSDKAccessToken.current().tokenString)
+                self.firebaseAuth(credential)
+            }
+        }
+    }
+    
+    func firebaseAuth(_ credential: AuthCredential) {
+        Auth.auth().signIn(with: credential, completion: { (user, error) in
+            if error != nil {
+                print("ADAM: Unable to authenticate with Firebase - \(error)")
+            }
+            else {
+                print("Adam: Successfully athenticated with Firebase")
+            }
+        })
+    }
+    
 }
 
